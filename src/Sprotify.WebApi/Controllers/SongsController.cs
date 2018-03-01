87d0d9dc6b1886c92085ac6a535659ef74fefa96
Wesley Cabus <wesley.cabus@realdolmen.com>
2018-03-01 10:17:46 +0100

    Moved SongsController to correct namespace

--- src/Sprotify.WebApi/Controllers/SongsController.cs
+++ src/Sprotify.WebApi/Controllers/SongsController.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Sprotify.Domain.Services;
 using Sprotify.WebApi.Models.Songs;
 
-namespace Sprotify.WebApi.Models
+namespace Sprotify.WebApi.Controllers
 {
     [Produces("application/json")]
     [Route("[controller]")]
